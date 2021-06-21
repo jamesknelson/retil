d2c7c9c7ec33f77927b2db5da875a0ee741a0469
James K Nelson <james@jamesknelson.com>
2021-06-21 11:51:24 +0900

    add more examples in new format

--- examples/retil-style/useMediaRenderer-emotion/app.tsx
+++ examples/retil-style/useMediaRenderer-emotion/app.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { defaultMediaQueries, useMediaRenderer } from 'retil-style'
 
 const App = () => {
--- examples/retil-style/useMediaRenderer-styled/app.tsx
+++ examples/retil-style/useMediaRenderer-styled/app.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from 'styled-components'
 import { defaultMediaQueries, useMediaRenderer } from 'retil-style'
 
