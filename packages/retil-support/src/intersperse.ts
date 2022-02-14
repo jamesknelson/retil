6765bcde2e623803291025d9b31523049450d818
James K Nelson <james@jamesknelson.com>
2022-02-14 16:05:50 +0900

    delete unused intersperse file

--- packages/retil-support/src/intersperse.ts
+++
@@ -1,9 +0,0 @@
-export const intersperse = <T>(separator: (n: number) => T, arr: T[]): T[] =>
-  arr.reduce<T[]>((acc, currentElement, currentIndex) => {
-    const isLast = currentIndex === arr.length - 1
-    return [
-      ...acc,
-      currentElement,
-      ...(isLast ? [] : [separator(currentIndex)]),
-    ]
-  }, [])
